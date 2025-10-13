8d4532461a1170ea6ac47716e401d0eb3cb14a89
Sharkzod <godstimeamerica99@gmail.com>
2025-10-13 22:05:26 +0100

    fixes

--- app/lib/api/apiClient.ts
+++ app/lib/api/apiClient.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'https://nimble-backend-jl6y.onrender.com',
+  baseURL: 'https://nimble-backend-jl6y.onrender.com/api',
   timeout: 10000,
   headers: {
     'Content-Type': 'application/json',
